250eb9d6137b4f96ea32dabad9f13715fb4c4eb3
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-08-18 22:22:29 -0400

    feat: add lightbox to detail product

--- next-project/src/components/e-commerce/ImagesDetailProduct.tsx
+++ next-project/src/components/e-commerce/ImagesDetailProduct.tsx
@@ -2,6 +2,9 @@
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { CarouselProductDetailImages } from './CarouselProductDetailImages';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 
 type Props = {
   images: string[];
@@ -9,6 +12,7 @@ type Props = {
 
 function ImagesDetailProduct({ images }: Props) {
   const [image, setImage] = useState<string>('');
+  const [openImage, setOpenImage] = useState<boolean>(false);
 
   useEffect(() => {
     setImage(images[0]);
@@ -19,7 +23,7 @@ function ImagesDetailProduct({ images }: Props) {
   };
   return (
     <div className="h-full w-full flex flex-col gap-5">
-      <div className="relative w-full aspect-[4/3] overflow-hidden rounded-2xl">
+      <div className="relative w-full aspect-[4/3] overflow-hidden rounded-2xl hover:cursor-pointer">
         {image && (
           <Image
             src={image}
@@ -27,9 +31,16 @@ function ImagesDetailProduct({ images }: Props) {
             fill
             className="transition-transform duration-500 group-hover:scale-125 object-cover"
             sizes="(max-width: 640px) 90vw, 400px"
+            onClick={() => setOpenImage(true)}
           />
         )}
       </div>
+      <Lightbox
+        open={openImage}
+        close={() => setOpenImage(false)}
+        slides={images.map((image) => ({ src: image }))}
+        plugins={[Zoom]}
+      />
 
       <div className="flex justify-center">
         <CarouselProductDetailImages
