2c84be92888199b63b4a64e7e00f3221772c39d0
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-09-08 20:27:50 -0400

    fix: update searchParams, become it as a async and await props

--- next-project/src/app/(user)/productos/page.tsx
+++ next-project/src/app/(user)/productos/page.tsx
@@ -1,25 +1,43 @@
-import React from "react";
-import Products from "@/components/user/products/Products";
-import { getAllProductsByCategory } from "@/services/product";
-import { getAllCategories } from "@/services/categories";
-import { unstable_noStore as noStore } from "next/cache";
-import Announcements from "@/components/user/products/Announcements";
+import React from 'react';
+import Products from '@/components/user/products/Products';
+import {
+  getAllProductsByCategory,
+  getAllAvailableProducts,
+} from '@/services/product';
+import { getAllCategories } from '@/services/categories';
+import { unstable_noStore as noStore } from 'next/cache';
+import Announcements from '@/components/user/products/Announcements';
 
-async function ProductsPage({
+export default async function ProductsPage({
   searchParams,
 }: {
-  searchParams: { category?: number; page?: string; limit?: string };
+  searchParams: Promise<{
+    category?: string;
+    page?: string;
+    limit?: string;
+  }>;
 }) {
-  noStore(); // Force SSR
+  noStore(); // fuerza SSR
 
-  const category = searchParams.category || "";
-  const currentPage = Number(searchParams.page) || 1;
-  const limit = Number(searchParams.limit) || 12;
+  const { category: categoryParam, limit: limitParam, page:pageParam } = await searchParams;
 
-  // fetch products depending on category
-  const { data, success } = category
-    ? await getAllProductsByCategory(category, limit, currentPage)
-    : await getAllProductsByCategory(0, limit, currentPage);
+  const categoryId = categoryParam ? Number(categoryParam) : null;
+  const currentPage = pageParam ? Number(pageParam) : 1;
+  const limit = limitParam ? Number(limitParam) : 12;
+
+  // Fetch products según categoría
+  let productsResponse;
+  if (categoryId) {
+    const { data, success } = await getAllProductsByCategory(
+      categoryId,
+      limit,
+      currentPage
+    );
+    if (success) productsResponse = data;
+  } else {
+    const { data, success } = await getAllAvailableProducts(currentPage, limit);
+    if (success) productsResponse = data;
+  }
 
   const { data: categories, success: categoriesSuccess } =
     await getAllCategories();
@@ -28,13 +46,11 @@ async function ProductsPage({
     <div className="flex flex-col">
       <Announcements />
       <Products
-        initialData={success && data ? data : null}
+        initialData={productsResponse || null}
         initialCategoriesData={
-          categories && categoriesSuccess ? categories : null
+          categoriesSuccess && categories ? categories : null
         }
       />
     </div>
   );
 }
-
-export default ProductsPage;
